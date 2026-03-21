e346100d4c4541b61f07d53c9ed95b07c670f7fe
Jared Usher <usherjared@yahoo.com>
2026-03-21 14:53:37 -0400

    Initial commit

---
+++ mob.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+#annotation shorthand for ready function:
+@onready var player = get_node("/root/Game/Player")
+#Gauruntees given node and all children have been created
+#func _ready():
+	#player = get_node("/root/Game/Player")
+
+
+func _physics_process(delta: float) -> void:
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * 300.0
+	move_and_slide()
